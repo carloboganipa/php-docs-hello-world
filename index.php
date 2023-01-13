1857706963e51579c4f74bdc966f6af7de31c8fa
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-13 21:24:07 +0100

    connect to database

--- index.php
+++ index.php
@@ -69,15 +69,11 @@
         $servername = "mariadbprojiac.mariadb.database.azure.com";
         $username = "project_admin@mariadbprojiac";
         $password = "I]nfrastructure23";
+        $database = "iac";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
+        $con=mysqli_init(); 
+        mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+        mysqli_real_connect($con, $servername, $username, $password, $database, 3306);
       ?>
 
   </div>
