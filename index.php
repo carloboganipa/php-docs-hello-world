b7b86c4cb54d04a4b7142e87aa86ed181a5b989f
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-13 21:25:41 +0100

    echo ciao

--- index.php
+++ index.php
@@ -71,9 +71,11 @@
         $password = "I]nfrastructure23";
         $database = "iac";
 
-        $con=mysqli_init(); 
+        /*$con=mysqli_init(); 
         mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-        mysqli_real_connect($con, $servername, $username, $password, $database, 3306);
+        mysqli_real_connect($con, $servername, $username, $password, $database, 3306);*/
+
+    echo 'ciao';
       ?>
 
   </div>
