4fa8c6bcbeadb602393919d5c1be6fdf8f1b8791
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-15 20:46:08 +0100

    cagzo

--- addWord.php
+++ addWord.php
@@ -14,7 +14,7 @@
 
     if(isset($_POST['submit']))
     {    
-        $$word = $_POST['word'];
+        $word = $_POST['word'];
         $sql = "INSERT INTO $table(text) VALUES ('$word')";
         if (mysqli_query($conn, $sql)) {
             echo "New record has been added successfully !";
