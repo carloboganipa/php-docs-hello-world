499b040f5571787e6b94da0f5f51331ea4948ee4
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-15 20:22:26 +0100

    solve php

--- index.php
+++ index.php
@@ -66,41 +66,6 @@
     <!-- PHP Code-->
 
     <?php
-
-    $host = 'mariadbprojiac.mariadb.database.azure.com';
-    $username = 'project_admin@mariadbprojiac';
-    $password = 'I]nfrastructure23';
-    $db_name = 'iac';
-    $table = 'users_words';
-
-    //Initializes MySQLi
-    $conn = mysqli_init();
-
-    // Establish the connection
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-
-    //If connection failed, show the error
-    if (mysqli_connect_errno())
-    {
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
-    }
-
-    // Taking word from form
-    $word = $_REQUEST['word'];
-
-    //INSERT INTO users_words(text) VALUES ('TIO');
-
-    $sql = "INSERT INTO $table(text) VALUES ('$word')";
-    
-     // Check if the query is successful
-     if(mysqli_query($conn, $sql)){
-      echo "<h3>data stored in a database successfully/h3>";
-  } else{
-      echo mysqli_error($conn);
-  }
-
-  // Close connection
-  mysqli_close($conn);
   ?>
 
   </div>
