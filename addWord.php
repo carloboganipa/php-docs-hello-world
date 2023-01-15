1ea420f8c17a431d3e5a335656be783cc1af5989
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-15 21:02:29 +0100

    res

--- addWord.php
+++ addWord.php
@@ -1,30 +1,4 @@
-
-<?php 
-    $host = 'mariadbprojiac.mariadb.database.azure.com';
-    $username = 'project_admin@mariadbprojiac';
-    $password = 'I]nfrastructure23';
-    $db_name = 'iac';
-    $table = 'users_words';
-
-    //Initializes MySQLi
-    $conn = mysqli_init();
-
-    // Establish the connection
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-
-    if(isset($_POST['submit']))
-    {    
-        $word = $_POST['word'];
-        $sql = "INSERT INTO $table(text) VALUES ('$word')";
-        if (mysqli_query($conn, $sql)) {
-            echo "New record has been added successfully !";
-        } else {
-            echo "Error: " . $sql . ":-" . mysqli_error($conn);
-        }
-        mysqli_close($conn);
-    }
-  ?>
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <title>IaC 2022/23</title>
@@ -81,6 +55,31 @@
   <h3>Projet IaC Monitoring - ID-2a 2022/2023 </h3>
   <div class="content">
     <!-- content-->
+    <?php 
+    $host = 'mariadbprojiac.mariadb.database.azure.com';
+    $username = 'project_admin@mariadbprojiac';
+    $password = 'I]nfrastructure23';
+    $db_name = 'iac';
+    $table = 'users_words';
+
+    //Initializes MySQLi
+    $conn = mysqli_init();
+
+    // Establish the connection
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
+    if(isset($_POST['submit']))
+    {    
+        $word = $_POST['word'];
+        $sql = "INSERT INTO $table(text) VALUES ('$word')";
+        if (mysqli_query($conn, $sql)) {
+            echo "New record has been added successfully !";
+        } else {
+            echo "Error: " . $sql . ":-" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    }
+  ?>
   </div>
 </div>
 
