f81fe27bc96f6df6629c4cbcefae16f533f13ba6
carlo.boganipa <carlo.boganipa@hes-so.ch>
2023-01-15 21:16:27 +0100

    corr

--- words.php
+++ words.php
@@ -94,7 +94,7 @@
     echo "<table>"; // start a table tag in the HTML
 
     while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-    echo "<tr><td>" . htmlspecialchars($row['word']) . "</tr>";  //$row['index'] the index here is a field name
+    echo "<tr><td>" . htmlspecialchars($row['text']) . "</tr>";  //$row['index'] the index here is a field name
     }
 
     echo "</table>"; //Close the table in HTML
